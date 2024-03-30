68a74171ec54fc084cdabf8538c9ab7c9aa04ade
Eduardo M. Santos <160991364+supitsdu@users.noreply.github.com>
2024-03-30 00:52:48 -0300

    fix(colorFormatter): invalid formats, plus refactor code logic

--- src/colorFormatter.js
+++ src/colorFormatter.js
@@ -3,9 +3,10 @@ import { Round } from './colorNormalizer'
 // A class for formatting color values into various formats.
 class ColorFormatter {
   #spacer
-  #alphaSpacer
   #percent
   #degree
+  #suffix
+  #alpha
 
   /**
    * Constructs a ColorFormatter object.
@@ -15,33 +16,33 @@ class ColorFormatter {
    */
   constructor({ minify, CSSNext } = {}) {
     const space = minify !== true ? ' ' : ''
+    const alphaSpacer = CSSNext === true ? `${space}/${space}` : `,${space}`
+
     this.#spacer = CSSNext === true ? ' ' : `,${space}`
-    this.#alphaSpacer = CSSNext === true ? `${space}/${space}` : space
     this.#percent = minify !== true ? '%' : ''
     this.#degree = minify !== true ? '°' : ''
+    this.#suffix = a => (a == 1 || CSSNext == true ? '' : 'a')
+    this.#alpha = a => (a == 1 ? '' : `${alphaSpacer}${a}`)
   }
-
-  #alphaCh = a => (a == 1 ? '' : `${this.#alphaSpacer}${a}`)
-  #suffix = a => (a == 1 ? '' : 'a')
 
   rgb(input) {
     const { r, g, b, a } = Round.rgb(input)
-    return `rgb${this.#suffix(a)}(${r}${this.#spacer}${g}${this.#spacer}${b}${this.#alphaCh(a)})`
+    return `rgb${this.#suffix(a)}(${r}${this.#spacer}${g}${this.#spacer}${b}${this.#alpha(a)})`
   }
 
   hsl(input) {
     const { h, s, l, a } = Round.hsl(input)
-    return `hsl${this.#suffix(a)}(${h}${this.#degree}${this.#spacer}${s}${this.#percent}${this.#spacer}${l}${this.#percent}${this.#alphaCh(a)})`
+    return `hsl${this.#suffix(a)}(${h}${this.#degree}${this.#spacer}${s}${this.#percent}${this.#spacer}${l}${this.#percent}${this.#alpha(a)})`
   }
 
   hsv(input) {
     const { h, s, v, a } = Round.hsv(input)
-    return `hsl${this.#suffix(a)}(${h}${this.#degree}${this.#spacer}${s}${this.#percent}${this.#spacer}${v}${this.#percent}${this.#alphaCh(a)})`
+    return `hsv${this.#suffix(a)}(${h}${this.#degree}${this.#spacer}${s}${this.#percent}${this.#spacer}${v}${this.#percent}${this.#alpha(a)})`
   }
 
   cmyk(input) {
     const { c, m, k, y, a } = Round.cmyk(input)
-    return `hsl${this.#suffix(a)}(${c}${this.#percent}${this.#spacer}${m}${this.#percent}${this.#spacer}${y}${this.#percent}${this.#spacer}${k}${this.#percent}${this.#alphaCh(a)})`
+    return `cmyk${this.#suffix(a)}(${c}${this.#percent}${this.#spacer}${m}${this.#percent}${this.#spacer}${y}${this.#percent}${this.#spacer}${k}${this.#percent}${this.#alpha(a)})`
   }
 }
 
