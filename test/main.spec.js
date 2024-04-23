76c65c17c1d5ab8db59d3c3dc079f3750dcd3083
Eduardo M. Santos <160991364+supitsdu@users.noreply.github.com>
2024-04-23 16:45:42 -0300

    test(Colorus): add tests for `Colorus.desaturate()` method

--- test/main.spec.js
+++ test/main.spec.js
@@ -25,3 +25,29 @@ describe('Colorus.darken()', () => {
     expect(new Colorus('#666').darken(0.6).toHex()).toBe('#292929')
   })
 })
+
+describe('Colorus.desaturate()', () => {
+  it('desaturates by default amount', () => {
+    expect(new Colorus('#3646d1').desaturate().toHex()).toBe('#3E4CC9')
+  })
+
+  it('desaturates by a specific amount', () => {
+    expect(new Colorus('#3646d1').desaturate(0.2).toHex()).toBe('#4652C1')
+  })
+
+  it('desaturates to max value from current lightness value', () => {
+    expect(new Colorus('#3646d1').desaturate(1).toHex()).toBe('#848484')
+  })
+
+  it('desaturates by a specific amount', () => {
+    expect(new Colorus('#3646d1').desaturate(0.2).toHex()).toBe('#4652C1')
+  })
+
+  it('desaturating of a light color', () => {
+    expect(new Colorus('#FFF').desaturate().toHex()).toBe('#FFFFFF')
+  })
+
+  it('desaturating by a large amount', () => {
+    expect(new Colorus('#3646d1').desaturate(0.8).toHex()).toBe('#747793')
+  })
+})
