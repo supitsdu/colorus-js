6a8e2df17cd38a20912fceb134e7a48c53bb93e4
Eduardo M. Santos <160991364+supitsdu@users.noreply.github.com>
2024-06-08 14:35:23 -0300

    docs: update and refine TSDoc for Colorus module
    - Documented RgbObject, HslObject, HsvObject, and CmykObject interfaces
    - Added AnyColorType, AnyColorObject, and AnyColor type definitions
    - Introduced FormatOptions and HexFormatOptions interfaces with detailed descriptions
    - Updated Colorus class documentation with new methods and refined descriptions
    - Provided comprehensive examples for each method and property
    - Adjusted method descriptions to specify string return types for toRgb, toHsl, toHsv, and toCmyk
    - Enhanced clarity on default values and behavior of methods
    
    This update enhances the clarity and usability of the Colorus module by providing thorough and structured documentation.

--- @types/main.d.ts
+++ @types/main.d.ts
@@ -2,13 +2,34 @@
  * The `colorus-js` module allows for easy manipulation and conversion of colors between different formats.
  */
 declare module 'colorus-js' {
-  export type RgbObject = { r: number | string; g: number | string; b: number | string; a?: number | string }
+  export interface RgbObject {
+    r: number | string
+    g: number | string
+    b: number | string
+    a?: number | string
+  }
 
-  export type HslObject = { h: number | string; s: number | string; l: number | string; a?: number | string }
+  export interface HslObject {
+    h: number | string
+    s: number | string
+    l: number | string
+    a?: number | string
+  }
 
-  export type HsvObject = { h: number | string; s: number | string; v: number | string; a?: number | string }
+  export interface HsvObject {
+    h: number | string
+    s: number | string
+    v: number | string
+    a?: number | string
+  }
 
-  export type CmykObject = { c: number | string; m: number | string; y: number | string; k: number | string; a?: number | string }
+  export interface CmykObject {
+    c: number | string
+    m: number | string
+    y: number | string
+    k: number | string
+    a?: number | string
+  }
 
   export type AnyColorType = 'rgb' | 'hsl' | 'hsv' | 'cmyk'
 
@@ -16,7 +37,7 @@ declare module 'colorus-js' {
 
   export type AnyColor = string | AnyColorObject
 
-  type FormatOptions = {
+  export interface FormatOptions {
     /**
      * Whether to attempt to minify the output.
      * @example
@@ -34,7 +55,7 @@ declare module 'colorus-js' {
     CSSNext?: boolean
   }
 
-  type HexFormatOptions = {
+  export interface HexFormatOptions {
     /**
      * Whether to attempt to minify the output.
      * @example
@@ -69,13 +90,14 @@ declare module 'colorus-js' {
      */
     constructor(input?: AnyColor)
 
-    /** Analitycal method to quickly tests the `input` for any valid color.
+    /**
+     * Analytical method to quickly test the `input` for any valid color.
      * @param input - The color input string or object.
-     * @return The type of the color (e.g.: `'rgb'`) if color is valid, otherwise `null`.
+     * @returns The type of the color (e.g.: `'rgb'`) if the color is valid, otherwise `null`.
      * @example
-     * Colorus.test('#F33') // Returns: 'hex'
-     * Colorus.test({ r: 255, g: 0, b: 0 }) // Returns: 'rgb'
-     * Colorus.test('#c(') // Returns: null
+     * Colorus.test('#F33'); // Returns: 'hex'
+     * Colorus.test({ r: 255, g: 0, b: 0 }); // Returns: 'rgb'
+     * Colorus.test('#c('); // Returns: null
      */
     static test(input?: unknown): AnyColorType | null
 
@@ -85,7 +107,7 @@ declare module 'colorus-js' {
     /**
      * Get the relative luminance of the current color.
      * @see {@link https://www.w3.org/TR/WCAG20-TECHS/G17.html | Techniques for WCAG 2.0 - G17}
-     * @return The relative luminance.
+     * @returns The relative luminance.
      */
     get luminance(): number
 
@@ -104,37 +126,37 @@ declare module 'colorus-js' {
     /**
      * Convert the current color to hexadecimal format.
      * @param options Formatting options.
-     * @return The hexadecimal representation of the color.
+     * @returns The hexadecimal representation of the color.
      * @example
-     * new Colorus('rgb(0, 0, 0)').toHex({ minify: true }) // Returns: '#000'
+     * new Colorus('rgb(0, 0, 0)').toHex({ minify: true }); // Returns: '#000'
      */
     toHex(options?: HexFormatOptions): string
 
     /**
-     * Convert the current color to RgbObject format.
+     * Convert the current color to `RGB` string format.
      * @param options Formatting options.
-     * @return The RgbObject representation of the color.
+     * @returns The `RGB` string representation of the color.
      */
     toRgb(options?: FormatOptions): string
 
     /**
-     * Convert the current color to HslObject format.
+     * Convert the current color to `HSL` string format.
      * @param options Formatting options.
-     * @return The HslObject representation of the color.
+     * @returns The `HSL` representation of the color.
      */
     toHsl(options?: FormatOptions): string
 
     /**
-     * Convert the current color to HsvObject format.
+     * Convert the current color to `HSV` string format.
      * @param options Formatting options.
-     * @return The HsvObject representation of the color.
+     * @returns The `HSV` string representation of the color.
      */
     toHsv(options?: FormatOptions): string
 
     /**
-     * Convert the current color to CmykObject format.
+     * Convert the current color to `CMYK` string format.
      * @param options Formatting options.
-     * @return The CmykObject representation of the color.
+     * @returns The `CMYK` string representation of the color.
      */
     toCmyk(options?: FormatOptions): string
 
@@ -142,76 +164,76 @@ declare module 'colorus-js' {
      * Mixes the current color with another color.
      * @param input The color to mix with.
      * @param amount The amount of mixing. (Default: `0.1`)
-     * @return A new Colorus instance representing the mixed color.
+     * @returns A new Colorus instance representing the mixed color.
      * @example
-     * const color = new Colorus('#f00')
-     * color.mix('#fff9e6', 0.45).toHex() // Returns: '#FF7068'
+     * const color = new Colorus('#f00');
+     * color.mix('#fff9e6', 0.45).toHex(); // Returns: '#FF7068'
      */
     mix(input: AnyColor, amount?: number): Colorus
 
     /**
      * Lightens the current color.
      * @param amount The amount of lightening. (Default: `0.1`)
-     * @return A new Colorus instance representing the lightened color.
+     * @returns A new Colorus instance representing the lightened color.
      */
     lighten(amount?: number): Colorus
 
     /**
      * Darkens the current color.
      * @param amount The amount of darkening.
-     * @return A new Colorus instance representing the darkened color.
+     * @returns A new Colorus instance representing the darkened color.
      */
     darken(amount?: number): Colorus
 
     /**
      * Saturates the current color.
      * @param amount The amount of saturation. (Default: `0.1`)
-     * @return A new Colorus instance representing the saturated color.
+     * @returns A new Colorus instance representing the saturated color.
      */
     saturate(amount?: number): Colorus
 
     /**
      * Desaturates the current color.
-     * @param amount - The amount of desaturation.
-     * @return A new Colorus instance representing the desaturated color.
+     * @param amount The amount of desaturation.
+     * @returns A new Colorus instance representing the desaturated color.
      */
     desaturate(amount?: number): Colorus
 
     /**
      * Changes the hue of the current color.
      * @param amount The amount of hue change. (Default: `0.1`)
-     * @return A new Colorus instance representing the color with changed hue.
+     * @returns A new Colorus instance representing the color with changed hue.
      */
     hue(amount?: number): Colorus
 
     /**
      * Changes the alpha (opacity) of the current color.
      * @param amount The amount of alpha change. (Default: `0.1`)
-     * @return A new Colorus instance representing the color with changed alpha.
+     * @returns A new Colorus instance representing the color with changed alpha.
      */
     alpha(amount?: number): Colorus
 
     /**
-     * Gets the contrast ratio between an foreground color and its adjacent background.
-     * @param backgroundColor the background color.
-     * @return The contrast ratio between the instantiated color and provided background color.
+     * Gets the contrast ratio between the foreground color and its adjacent background.
+     * @param backgroundColor The background color.
+     * @returns The contrast ratio between the instantiated color and the provided background color.
      * @example
-     * new Colorus('#000').contrastRatio('#f3f3f3') // Returns: 18.92
+     * new Colorus('#000').contrastRatio('#f3f3f3'); // Returns: 18.92
      */
     contrastRatio(backgroundColor: AnyColor): number
 
     /**
      * Inverts the color using sRGB values.
-     * @return A new Colorus instance representing the color with inverted color values.
+     * @returns A new Colorus instance representing the color with inverted color values.
      */
     invert(): Colorus
 
     /**
      * Converts the current color to grayscale.
-     * @param useNTSCFormula - Whether to use the NTSC formula for conversion. (Default: `false`)
-     * @return A new Colorus object representing the grayscale color.
+     * @param useNTSCFormula Whether to use the NTSC formula for conversion. (Default: `false`)
+     * @returns A new Colorus instance representing the grayscale color.
      * @example
-     * // Create a Colorus object representing an RGB color
+     * // Create a Colorus instance representing an RGB color
      * const color = new Colorus('rgb(50 168 82)');
      * color.grayscale().toRgb(); // Returns: 'rgb(137, 137, 137)'
      * // Convert using the NTSC formula
