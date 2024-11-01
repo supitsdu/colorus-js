e29540625798efc75d9843dc250207a0634b854c
Eduardo M. Santos <contact@supitsdu.me>
2024-11-01 19:54:11 -0300

    fix(plugins/invert): should pass options to new plugin instance

--- src/plugins/invert.ts
+++ src/plugins/invert.ts
@@ -2,10 +2,13 @@ import { dye } from "../dye";
 import { createPlugin } from "../utils/pluginHelpers";
 
 export const invert = createPlugin("invert", function () {
-	return dye({
-		r: 255 - this.rgb.r,
-		g: 255 - this.rgb.g,
-		b: 255 - this.rgb.b,
-		a: this.rgb.a,
-	});
+	return dye(
+		{
+			r: 255 - this.rgb.r,
+			g: 255 - this.rgb.g,
+			b: 255 - this.rgb.b,
+			a: this.rgb.a,
+		},
+		this.options,
+	);
 });
