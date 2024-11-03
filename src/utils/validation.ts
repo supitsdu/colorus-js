b01b04f7c8f7f384a800a7574fd3c763556f311e
Eduardo M. Santos <contact@supitsdu.me>
2024-11-03 16:58:21 -0300

    refact(validation): remove unused validation utils

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,16 +1,3 @@
-import {
-	chCmyk,
-	chHsl,
-	chHsv,
-	chRgb,
-	regexCmyk,
-	regexHex,
-	regexHsl,
-	regexHsv,
-	regexRgb,
-} from "../patterns";
-import type { Colors } from "../types";
-
 /**
  * Checks if the input value is a dictionary-like object with key-value pairs.
  * @param v - The value to check against.
@@ -31,36 +18,3 @@ export const isColorValue = (v: unknown): boolean => {
 		return isColorValue(Number.parseFloat(v)) && isColorValue(Number(v));
 	return false;
 };
-
-/**
- * Creates a color analyzer function that checks if the input value is a color model.
- * @param keys - The keys to check against.
- * @param exp - The regular expression to match the color string.
- * @return The model checker function.
- */
-export const createColorAnalyzer =
-	(keys: string[] | string, exp: RegExp) =>
-	<T = unknown>(v: T): string | null => {
-		if (isObject(v)) {
-			for (const key of keys) {
-				if (
-					!(
-						isColorValue((v as Colors.Object<any>)[key]) ||
-						(key === "a" && (v as Colors.Object<any>)[key] === undefined)
-					)
-				) {
-					return null;
-				}
-			}
-		} else if (typeof v !== "string" || !exp.test(v)) {
-			return null;
-		}
-
-		return typeof keys === "string" ? keys : keys.join("");
-	};
-
-export const testHex = createColorAnalyzer("hexa", regexHex);
-export const testRgb = createColorAnalyzer(chRgb, regexRgb);
-export const testHsl = createColorAnalyzer(chHsl, regexHsl);
-export const testHsv = createColorAnalyzer(chHsv, regexHsv);
-export const testCmyk = createColorAnalyzer(chCmyk, regexCmyk);
